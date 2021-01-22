8176493feaea1cb3420db5756ed3983a7c399575
Krishna Khadka <krishna.khadka@mavs.uta.edu>
2021-01-21 20:23:34 -0600

    Initial commit

---
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -0,0 +1,40 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String psoterPath;
+    String title;
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        psoterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+    }
+
+    public static List<Movie> fromJSONArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for(int i = 0; i<movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+
+    public String getPsoterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s",psoterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
